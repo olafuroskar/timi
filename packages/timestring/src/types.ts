3b3df852fe2aae75ccf4e648c847ca79a4fe9abf
olafuroskar <ol.os.om.97@gmail.com>
2022-08-22 19:34:41 +0000

    Update types.ts

--- packages/timestring/src/types.ts
+++ packages/timestring/src/types.ts
@@ -5,31 +5,28 @@ export enum TimeType {
   WithHoursAndMs
 }
 
-export type Time =
-  | {
-      type: TimeType.Default;
-      minutes: number;
-      seconds: number;
-    }
-  | {
-      type: TimeType.WithHours;
-      hours: number;
-      minutes: number;
-      seconds: number;
-    }
-  | {
-      type: TimeType.WithMs;
-      minutes: number;
-      seconds: number;
-      milliseconds: number;
-    }
-  | {
-      type: TimeType.WithHoursAndMs;
-      hours: number;
-      minutes: number;
-      seconds: number;
-      milliseconds: number;
-    };
+export type TimeDefault = { type: TimeType.Default; minutes: number; seconds: number };
+export type TimeWithHours = {
+  type: TimeType.WithHours;
+  hours: number;
+  minutes: number;
+  seconds: number;
+};
+export type TimeWithMs = {
+  type: TimeType.WithMs;
+  minutes: number;
+  seconds: number;
+  milliseconds: number;
+};
+export type TimeWithHoursAndMs = {
+  type: TimeType.WithHoursAndMs;
+  hours: number;
+  minutes: number;
+  seconds: number;
+  milliseconds: number;
+};
+
+export type Time = TimeDefault | TimeWithHours | TimeWithMs | TimeWithHoursAndMs;
 
 export type TimeAndTimestring = { time: Time; timeString: string };
 
